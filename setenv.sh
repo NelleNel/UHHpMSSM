38d38f3437daf9278142059af5a7112c24e81a1c
Lukas <lukas.vanelderen@cern.ch>
2014-10-31 10:32:00 +0100

    update

---
+++ setenv.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+shopt -s expand_aliases
+module ()
+{
+    eval `/usr/bin/modulecmd bash $*`
+}
+module use -a /afs/desy.de/group/cms/modulefiles/
+module load root/5.34
+
+# variables used for job distribution
+export MYPROJECTDIR=$HOME/UHHpMSSM
+export SWDIR=$MYPROJECTDIR/software
+export ANASCRIPTS=$SWDIR/anascripts
+export PATH=$PATH:$ANASCRIPTS:$MYPROJECTDIR/scripts:$SWDIR/grid-control
+export PYTHONPATH=$PYTHONPATH:$ANASCRIPTS
+export ENVSCRIPT=$MYPROJECTDIR/setenv.sh
